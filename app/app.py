25cadff0f009683879c66cfe721df0b41cc132d8
Jonathan <jonathandrewkerr@gmail.com>
2019-04-26 14:56:13 +0100

    Add Flask-SQLAlchemy

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 
 @app.route('/')
