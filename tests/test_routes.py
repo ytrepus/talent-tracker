0d26cd9f504c5a67b7427ac89f5927507cad7624
Jonathan Kerr <jonathandrewkerr@gmail.com>
2019-05-15 21:32:02 +0100

    Add first basic test
    Does the index exist? It does

---
+++ tests/test_routes.py
@@ -0,0 +1,7 @@
+def test_home_status_code(test_client):
+    # sends HTTP GET request to the application
+    # on the specified path
+    result = test_client.get('/')
+
+    # assert the status code of the response
+    assert result.status_code == 200
