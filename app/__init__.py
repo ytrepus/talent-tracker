9fa9f5a29e22eb0c274602f72ea510b594304315
Jonathan <jonathandrewkerr@gmail.com>
2019-05-17 12:32:56 +0100

    Test passes
    I've added code to allow for uploads and for the transformation into leadership questions. I think this route will need to be extended to cater for other bulk uploads but for now it passes the tests!

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from app.models import db
+import secrets
 
 import os
 
@@ -8,8 +9,11 @@ migrate = Migrate()
 
 
 def create_app():
+    UPLOAD_FOLDER = 'app/uploads'
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['SECRET_KEY'] = secrets.token_urlsafe(64)
     db.init_app(app)
     migrate.init_app(app, db)
 
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,6 +1,17 @@
-from flask import render_template
-from app.models import Candidate
+from flask import render_template, request, flash, redirect, current_app, url_for
+from werkzeug.utils import secure_filename
+from app.models import Candidate, FLSLeadership, SLSLeadership, db
 from app.routes import route_blueprint
+import os
+import csv
+
+
+UPDATE_TYPES = {"FLS": FLSLeadership, "SLS": SLSLeadership}
+ALLOWED_TYPES = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_TYPES
 
 
 @route_blueprint.route('/')
@@ -20,6 +31,27 @@ def results():
         {'heading': 'Heading', 'content': 'Lorem ipsum, blah blah'}
     ])
 
+
 @route_blueprint.route('/update/bulk', methods=['POST', 'GET'])
 def update_bulk():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('uploaded_file',
+            #                         filename=filename))
+            with open(os.path.join(current_app.config['UPLOAD_FOLDER'], filename)) as uploaded_csv:
+                update_type = UPDATE_TYPES.get(request.form.get('update_type'))
+                csv_reader = csv.DictReader(uploaded_csv, delimiter=',')
+                for row in csv_reader:
+                    db.session.add(update_type(**row))
+                    db.session.commit()
     return ("Posted", 200)
