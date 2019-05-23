2f0400b9539d7c7a4a1fac8f30962148373b4f38
Jonathan <jonathandrewkerr@gmail.com>
2019-05-23 17:39:20 +0100

    Reduce extra code in view
    Automagic generation of promotion grades!

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, url_for, redirect
-from app.models import Candidate
+from app.models import Candidate, Grade
 from app.routes import route_blueprint
 
 
@@ -28,10 +28,13 @@ def choose_update():
                                 update_type=request.form.get("update-type")))
     return render_template('choose_update.html')
 
+
 @route_blueprint.route('/update/<string:bulk_or_single>/<string:update_type>')
 def update(bulk_or_single, update_type):
     update_types = {
-        "role": "Role update", "fls-survey": "FLS Survey update", "sls-survey": "SLS Survey update"
+        "role": {'title': "Role update", "promotable_grades": Grade.promotion_roles(Grade(value='Grade name', rank=7))},
+        "fls-survey": "FLS Survey update", "sls-survey": "SLS Survey update"
     }
     template = f"updates/{bulk_or_single}-{update_type}.html"
-    return render_template(template, page_header=update_types.get(update_type))
+    return render_template(template, page_header=update_types.get(update_type).get('title'),
+                           data=update_types.get(update_type))
