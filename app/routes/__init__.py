4fb47762a4ee170715d341b040c2508bef13a65b
Jonathan <jonathandrewkerr@gmail.com>
2019-05-31 10:28:16 +0100

    Implement lockdown
    All routes are now locked down to authenticated users only

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
+from flask_login import current_user
 
 route_blueprint = Blueprint('route_blueprint', __name__)
+
+
+@route_blueprint.before_request
+def restrict_to_logged_in_users():
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+
 
 from app.routes import routes
