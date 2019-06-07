b31c0ca3135c2783b9f9be0824483ffa8eb28e06
Jonathan <jonathandrewkerr@gmail.com>
2019-06-07 15:46:18 +0100

    Change DB to allow names

--- app/models.py
+++ app/models.py
@@ -45,6 +45,8 @@ class Candidate(db.Model):
     personal_email = db.Column(db.String(120), unique=True)
     joining_date = db.Column(db.Date())
     completed_fast_stream = db.Column(db.Boolean())
+    first_name = db.Column(db.String(128))
+    last_name = db.Column(db.String(128))
 
     joining_grade = db.Column(db.ForeignKey('grade.id'))
     ethnicity_id = db.Column(db.ForeignKey('ethnicity.id'))
