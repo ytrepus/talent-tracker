0b0e1a7867328aa85c82377877387e3ced692b31
Jonathan <jonathandrewkerr@gmail.com>
2019-06-21 23:36:07 +0100

    Add test that deferments aren't included in reports
    These are reasonably good tests to check that if candidates defer they're not included in the promotions numbers

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,7 +1,8 @@
 import pytest
 from typing import List
-from reporting.reports import CharacteristicPromotionReport, BooleanCharacteristicPromotionReport
-from app.models import Ethnicity, Candidate
+from reporting.reports import CharacteristicPromotionReport, BooleanCharacteristicPromotionReport, PromotionReport
+from app.models import Ethnicity, Candidate, Application
+from datetime import date
 
 
 class TestReports:
@@ -32,6 +33,52 @@ class TestReports:
         output = CharacteristicPromotionReport(*parameters).return_data()
         assert output.data.decode("UTF-8").split('\n') == expected_output
 
+    def test_deferred_candidates_are_not_counted(self, test_session, candidates_promoter):
+        """
+        Two candidates apply to the 2019 intake and are successful. One defers to 2019. Both are promoted in the
+        following year. How many "successes" should be counted for the programme? The answer should be one, because only
+        one is actually on the programme at the time
+        """
+        test_session.add(Ethnicity(id=1, value="Prefer not to say"))
+        test_session.commit()
+
+        candidates = [Candidate(ethnicity_id=1) for i in range(2)]
+        # all candidates apply for the 2019 intake and are successful
+        for candidate in candidates:
+            candidate.applications.append(
+                Application(scheme_id=1, application_date=date(2018, 6, 1), scheme_start_date=date(2019, 3, 1),
+                            successful=True)
+            )
+        # all candidates are substantively promoted
+        candidates_promoter(candidates, 1, temporary=False)
+        # one candidate defers to the 2020 intake
+        candidates[0].applications[0].defer(date_to_defer_to=date(2020, 3, 1))
+        # save to the database
+        test_session.add_all(candidates)
+        test_session.commit()
+
+        data = CharacteristicPromotionReport('FLS', '2019', 'ethnicity').get_data()
+        expected_output = [['Prefer not to say', 1, 1.0, 0, 0.0, 1]]
+        assert data == expected_output
+
+
+class TestPromotionReport:
+    def test_eligible_candidates(self, test_session, candidates_promoter):
+        test_session.add(Ethnicity(id=1, value="Prefer not to say"))
+        test_session.commit()
+
+        candidates = [Candidate(ethnicity_id=1) for i in range(2)]
+        for candidate in candidates:
+            candidate.applications.append(
+                Application(scheme_id=1, application_date=date(2018, 6, 1), scheme_start_date=date(2019, 3, 1))
+            )
+        test_session.add_all(candidates)
+        candidates[0].applications[0].defer(date_to_defer_to=date(2020, 3, 1))
+        test_session.commit()
+
+        data = PromotionReport('FLS', '2019').eligible_candidates()
+        assert len(data) == 1
+
 
 class TestBooleanCharacteristicPromotionReport:
     def test_get_data(self, disability_with_without_no_answer, candidates_promoter, scheme_appender, test_session):
@@ -50,8 +97,7 @@ class TestBooleanCharacteristicPromotionReport:
             scheme_appender(group, scheme_id_to_add=1)
 
         test_session.commit()
-
-        output = BooleanCharacteristicPromotionReport('FLS', '2018', 'long_term_health_condition').get_data()
+        output = BooleanCharacteristicPromotionReport('FLS', '2019', 'long_term_health_condition').get_data()
         expected_output = [
             ["People with a disability", 3, 0.3, 0, 0.0, 10],
             ["People without a disability", 4, 0.4, 0, 0.0, 10],
