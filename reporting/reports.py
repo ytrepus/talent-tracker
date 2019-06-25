517f193041c888a5465d47f92c6d3d204a48aa5f
Jonathan <jonathandrewkerr@gmail.com>
2019-06-25 22:02:39 +0100

    Implement Delta report
    At the moment this report only works for the Delta promotions. I'd like to see if I can refactor it for the meta promotions, or move some of the good code into a higher level class for use elsewhere

--- reporting/reports.py
+++ reporting/reports.py
@@ -1,5 +1,5 @@
 from datetime import date
-from typing import List
+from typing import List, Union, Tuple
 from sqlalchemy import and_
 
 from app.models import Ethnicity, Scheme, Gender, Candidate, Application, Sexuality, WorkingPattern, Belief, AgeRange
@@ -79,6 +79,9 @@ class PromotionReport(Report, ABC):
     def get_data(self):
         pass
 
+    def row_writer(self, characteristic) -> List:
+        pass
+
     def eligible_candidates(self) -> List[Candidate]:
         """
         Candidates eligible to be reported on have an application whose scheme start date is aligned with ```year``` and
@@ -87,12 +90,14 @@ class PromotionReport(Report, ABC):
         :return:
         :rtype:
         """
-        print(Application.query.filter(Application.scheme_start_date == self.intake_date,).all())
         eligible_applications = Application.query.filter(and_(
             Application.scheme_start_date == self.intake_date,
             Application.scheme_id == self.scheme.id
         )).all()
         return [application.candidate for application in eligible_applications]
+
+    def promoted_candidates(self, temporary, candidates: List[Candidate]):
+        return [candidate for candidate in candidates if candidate.promoted(self.promoted_before_date, temporary)]
 
     def write_row(self, row_data, data_object, csv_writer):
         csv_writer.writerow((
@@ -133,7 +138,7 @@ class CharacteristicPromotionReport(PromotionReport):
         total_candidates = len(eligible_candidates)
         return [len(promoted_candidates), self.decimal_or_none(len(promoted_candidates), total_candidates)]
 
-    def line_writer(self, characteristic):
+    def row_writer(self, characteristic):
         line = [f"{characteristic.value}"]
         line.extend(self.promoted_candidates_with_this_characteristic(characteristic, False))
         line.extend(self.promoted_candidates_with_this_characteristic(characteristic, True))
@@ -145,7 +150,7 @@ class CharacteristicPromotionReport(PromotionReport):
         output = []
         characteristics = self.table.query.all()
         for characteristic in characteristics:
-            output.append(self.line_writer(characteristic))
+            output.append(self.row_writer(characteristic))
         return output
 
 
@@ -165,9 +170,9 @@ class BooleanCharacteristicPromotionReport(PromotionReport):
 
     def get_data(self):
         characteristics = [True, False, None]
-        return [self.line_writer(characteristic) for characteristic in characteristics]
+        return [self.row_writer(characteristic) for characteristic in characteristics]
 
-    def line_writer(self, characteristic: bool):
+    def row_writer(self, characteristic: bool):
         line = [f"{self.human_readable_row_titles.get(characteristic)}"]
         line.extend(self.promoted_candidates_with_this_characteristic(characteristic, temporary=False))
         line.extend(self.promoted_candidates_with_this_characteristic(characteristic, temporary=True))
@@ -181,3 +186,89 @@ class BooleanCharacteristicPromotionReport(PromotionReport):
                                if candidate.promoted(self.promoted_before_date, temporary=temporary)]
         total_candidates = len(eligible_candidates)
         return [len(promoted_candidates), self.decimal_or_none(len(promoted_candidates), total_candidates)]
+
+
+class OfferPromotionReport(PromotionReport):
+    def __init__(self, scheme, year, attribute):
+        super().__init__(scheme, year, attribute)
+        self.upper_attribute = attribute.upper()
+        self.human_readable_row_titles = {
+            True: f"Candidates eligible for {self.upper_attribute}",
+            False: f"Candidates who took up {self.upper_attribute} offer"
+        }
+
+    def promoted_candidates_with_this_characteristic(self, characteristic: bool, temporary: bool) -> Tuple[int, float]:
+        """
+        The two outputs here are: the number of candidates promoted who are on meta/delta, and the number of
+        candidates promoted who were eligible for meta/delta
+        :param characteristic:
+        :type characteristic:
+        :param temporary:
+        :type temporary:
+        :return:
+        :rtype:
+        """
+        eligible_candidates = [candidate for candidate in self.eligible_candidates()
+                               if getattr(candidate.applications[0], self.attribute) == characteristic]
+        promoted_candidates = [candidate for candidate in eligible_candidates
+                               if candidate.promoted(self.promoted_before_date, temporary=temporary)]
+        total_candidates = len(eligible_candidates)
+        return len(promoted_candidates), self.decimal_or_none(len(promoted_candidates), total_candidates)
+
+    def row_writer(self, characteristic: bool) -> List[Union[str, int, float]]:
+        line = [f"{self.human_readable_row_titles.get(characteristic)}"]
+        line.extend(self.promoted_candidates_with_this_characteristic(characteristic, temporary=False))
+        line.extend(self.promoted_candidates_with_this_characteristic(characteristic, temporary=True))
+        return line
+
+
+class MetaOfferPromotionReport(OfferPromotionReport):
+    def __init__(self, scheme, year, attribute):
+        super().__init__(scheme, year, attribute)
+
+    def eligible_candidates(self) -> List[Candidate]:
+        return [candidate for candidate in super().eligible_candidates if candidate.ethnicity.bame]
+
+
+class DisabilityReport(PromotionReport):
+    def candidates_with_a_disability(self):
+        return [candidate for candidate in super().eligible_candidates() if candidate.long_term_health_condition]
+
+    def candidates_on_delta(self):
+        return [candidate for candidate in super().eligible_candidates() if candidate.applications[0].delta]
+
+
+class DeltaOfferPromotionReport(OfferPromotionReport, DisabilityReport):
+    def __init__(self, scheme, year, attribute):
+        super().__init__(scheme, year, attribute)
+
+    def eligible_candidates(self):
+        return self.candidates_with_a_disability()
+
+    def get_data(self):
+        """
+        This method generates only two rows of data. The first is people with a disability who've been promoted.
+        The second is people with a disability on DELTA who've been promoted
+        :return:
+        :rtype:
+        """
+        output = []
+        row_metadata = [
+            ("Candidates eligible for DELTA", self.candidates_with_a_disability),
+            ("Candidates on DELTA", self.candidates_on_delta)
+        ]
+        for tupl in row_metadata:
+            output.append(self.row_writer(tupl[0], tupl[1]))
+        return output
+
+    def row_writer(self, row_header: str, candidate_list_generator: callable):
+        output = [row_header]
+        total_candidates = candidate_list_generator()
+        output.extend(self.chunk_writer(False, total_candidates))
+        output.extend(self.chunk_writer(True, total_candidates))
+        output.append(len(total_candidates))
+        return output
+
+    def chunk_writer(self, temporary, candidates):
+        promoted_number = len(self.promoted_candidates(temporary, candidates))
+        return [promoted_number, self.decimal_or_none(promoted_number, len(candidates))]
