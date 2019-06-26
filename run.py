3b36585916e26afecb0508ee9ad73979068ce62e
Jonathan <jonathandrewkerr@gmail.com>
2019-06-26 20:17:45 +0100

    Add new 'seed' command
    Now you can `flask seed`!

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app
 from app.models import Role, User, Candidate, Organisation, db
+import click
+from modules.seed import commit_data
 
 app = create_app()
 
@@ -7,3 +9,14 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Role': Role, 'Candidate': Candidate, 'Organisation': Organisation}
+
+
+@app.cli.command()
+@click.option('--new-install', default=False, help='Build the database and seed it with data')
+def seed(new_install):
+    """
+    If you pass it 'new install' it'll build the database for you. Otherwise it'll just seed it with data
+    """
+    if new_install:
+        db.create_all()
+    commit_data()
