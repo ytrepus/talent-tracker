72a125f6c0f7f77b57b29a8c0793b5efa1cc1146
Jonathan <jonathandrewkerr@gmail.com>
2019-06-26 11:14:30 +0100

    Change interface to allow for DELTA report

--- app/reports/routes.py
+++ app/reports/routes.py
@@ -1,18 +1,22 @@
 from flask import request, render_template
 from app.reports import reports_bp
-from reporting.reports import CharacteristicPromotionReport, BooleanCharacteristicPromotionReport
+from reporting.reports import CharacteristicPromotionReport, BooleanCharacteristicPromotionReport, \
+    DeltaOfferPromotionReport, MetaOfferPromotionReport
 
 
 @reports_bp.route('/', methods=["POST", "GET"])
 def reports():
 
     def promotion_report_type(string_from_form):
-        characteristic_reports = ['ethnicity', 'gender', 'working-pattern', 'age-range', 'belief', 'sexuality']
+        characteristic_reports = ['ethnicity', 'gender', 'working_pattern', 'age_range', 'belief', 'sexuality']
         boolean_reports = ['long_term_health_condition', 'caring_responsibility']
+        offer_reports = {'delta': DeltaOfferPromotionReport, 'meta': MetaOfferPromotionReport}
         if string_from_form in characteristic_reports:
             return CharacteristicPromotionReport
         elif string_from_form in boolean_reports:
             return BooleanCharacteristicPromotionReport
+        elif string_from_form in offer_reports.keys():
+            return offer_reports.get(string_from_form)
 
     report_classes = {
         'promotions': promotion_report_type
