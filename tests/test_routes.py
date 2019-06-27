253dc6733212b7067833ec9fb326d69aa6e1512d
Jonathan <jonathandrewkerr@gmail.com>
2019-06-27 10:25:55 +0100

    Correct test
    Correct type in test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -146,6 +146,6 @@ class TestReports:
 
 
 class TestProfile:
-    def test_get(self, test_client, logged_in_user):
+    def test_get(self, test_client, logged_in_user, test_candidate):
         result = test_client.get('/candidates/candidate/1')
-        assert "Career profile for Test Candidate" in result.data.decode("utf-8")
+        assert "Career profile for Testy Candidate" in result.data.decode("utf-8")
