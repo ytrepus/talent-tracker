176cb884b1df12760e7b3a5fb1edbad12f9fed3e
Jonathan Kerr <jonathandrewkerr@gmail.com>
2019-06-30 08:48:00 +0100

    Implement report factory
    I've moved all the logic for creating reports out to a factory, making the controller clearer and reducing the amount of code that would need to be changed when another report is added

--- app/reports/routes.py
+++ app/reports/routes.py
@@ -1,32 +1,14 @@
 from flask import request, render_template
 from app.reports import reports_bp
-from reporting.reports import CharacteristicPromotionReport, BooleanCharacteristicPromotionReport, \
-    DeltaOfferPromotionReport, MetaOfferPromotionReport
+from reporting.reports import ReportFactory
 
 
 @reports_bp.route('/', methods=["POST", "GET"])
 def reports():
 
-    def promotion_report_type(string_from_form):
-        characteristic_reports = ['ethnicity', 'gender', 'working_pattern', 'age_range', 'belief', 'sexuality']
-        boolean_reports = ['long_term_health_condition', 'caring_responsibility']
-        offer_reports = {'delta': DeltaOfferPromotionReport, 'meta': MetaOfferPromotionReport}
-        if string_from_form in characteristic_reports:
-            return CharacteristicPromotionReport
-        elif string_from_form in boolean_reports:
-            return BooleanCharacteristicPromotionReport
-        elif string_from_form in offer_reports.keys():
-            return offer_reports.get(string_from_form)
-
-    report_classes = {
-        'promotions': promotion_report_type
-    }
-
     if request.method == "POST":
         form_data = request.form.to_dict()
-        report_type = report_classes.get(form_data.pop('report-type'))
-        report_class = report_type(form_data.get('attribute'))
-        params = form_data
-        initialised_class = report_class(**params)
-        return initialised_class.return_data()
+        report = ReportFactory.create_report(report_type=form_data.pop('report-type'), **form_data)
+        return report.return_data()
+
     return render_template("reports/select-report.html")
--- reporting/reports.py
+++ reporting/reports.py
@@ -212,3 +212,22 @@ class DeltaOfferPromotionReport(OfferPromotionReport):
 
     def eligible_candidates(self):
         return [candidate for candidate in super().eligible_candidates() if candidate.long_term_health_condition]
+
+
+class ReportFactory:
+    @staticmethod
+    def create_report(report_type: str, **kwargs) -> Report:
+
+        characteristic_reports = {key: CharacteristicPromotionReport for key in
+                                  ['ethnicity', 'gender', 'working_pattern', 'age_range', 'belief', 'sexuality']}
+        boolean_reports = {key: BooleanCharacteristicPromotionReport for key in
+                           ['long_term_health_condition', 'caring_responsibility']}
+        offer_reports = {'delta': DeltaOfferPromotionReport, 'meta': MetaOfferPromotionReport}
+        promotion_reports = {**characteristic_reports, **boolean_reports, **offer_reports}
+        reports = {"promotions": promotion_reports}
+
+        report = reports.get(report_type).get(kwargs.get('attribute'))
+        if not report:
+            raise NotImplementedError("No such report type exists")
+        else:
+            return report(**kwargs)
