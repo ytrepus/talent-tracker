7cd56b72ba4f46d6c2176b6aba32a78ece8ec9cc
Jonathan <jonathandrewkerr@gmail.com>
2019-07-04 11:52:40 +0100

    Fix two auth_bp errors

--- app/candidates/__init__.py
+++ app/candidates/__init__.py
@@ -7,7 +7,7 @@ candidates_bp = Blueprint('candidates', __name__)
 @candidates_bp.before_request
 def restrict_to_logged_in_users():
     if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('auth_bp.login'))
 
 
 from app.candidates import routes  # noqa: E402,F401
